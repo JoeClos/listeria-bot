a5b78745eee4be0606d74c6783f78031f54221f7
Josephine <josephine.closan@yahoo.com>
2022-11-09 14:28:51 +0200

    fetch and display query

--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,9 +1,34 @@
+import axios from "axios";
+import {useState, useEffect } from "react";
+
 const GetData = () => {
+    const url = "https://query.wikidata.org/sparql?format=json&query="
+    const sparql = document.location.search.slice(1);
+    const [ query, setQuery ] = useState({data: []})
+
+    useEffect(() => {
+        axios
+        .get(url + sparql)
+        .then((response) => {
+            setQuery(response.data)
+            console.log(response.data);
+            
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }, [])
+
     return(
         <div>
-
+            <pre>
+                {JSON.stringify(query, null, "   " ) 
+                           }
+            </pre>
+            
         </div>
     )
+    
 }
 
 export default GetData;
