7e068667b8d8ac5c9a07813504fdc6d1995b87f4
Josephine <josephine.closan@yahoo.com>
2022-11-16 14:33:50 +0200

    create a link to wikidata page

--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,35 +1,47 @@
 import axios from "axios";
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const GetData = () => {
-    const url = "https://query.wikidata.org/sparql?format=json&query="
-    const sparql = document.location.search.slice(1);
-    const [ query, setQuery ] = useState({data: []})
+  const url = "https://query.wikidata.org/sparql?format=json&query=";
+  const sparql = document.location.search.slice(1);
+  const queryWikidata = "https://query.wikidata.org/";
 
-    useEffect(() => {
-        axios
-        .get(url + sparql)
-        .then((response) => {
-            setQuery(response.data)
-            console.log(response.data);
-            
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }, [sparql])
 
-    return(
-        <div>
-            <h1>Listeria</h1>
-            <pre>
-                {JSON.stringify(query, null, "   " ) 
-                           }
-            </pre>
-            
-        </div>
-    )
-    
-}
+  const [query, setQuery] = useState({ data: [] });
+
+  useEffect(() => {
+    axios
+      .get(url + sparql)
+      .then((response) => {
+        setQuery(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [sparql]);
+
+  return (
+    <div>
+      <h1>Listeria</h1>
+
+            <a
+              href= {queryWikidata + "#" + sparql}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Link
+            </a>
+     
+
+
+      <pre>
+        {JSON.stringify(query, null, "   ")}
+      
+      </pre>
+     
+    </div>
+  );
+};
 
 export default GetData;
