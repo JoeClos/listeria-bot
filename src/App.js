c7b6913b738d7d68f3641698e6a1c7ece8bb98e6
Linh <linh.nguyenkhanhib@gmail.com>
2022-11-21 12:54:02 +0200

    styling loading section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
-import GetData from './components/GetData';
-
+import { useState } from "react";
+import GetData from "./components/GetData";
+import "./App.css";
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return (
+      <>
+      
+        <div className="position">
+          <div className="loader"></div>
+          <h3>Loading ...</h3>
+        </div>
+        
+      </>
+    );
+  }
   return (
     <div className="App">
       <GetData />
