dd3cf15a01ea3fa9d4f243599c7216c44c1dd6b3
Josephine <josephine.closan@yahoo.com>
2022-11-21 14:22:41 +0200

    import React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import GetData from "./components/GetData";
 import "./App.css";
 function App() {
--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import convert from "../convert";
 import Navbar from "./Navbar";
 import '../css/GetData.css'
