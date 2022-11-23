a37bd14ee02029c48d3d5d3784e4aa2d8f00f234
Josephine <josephine.closan@yahoo.com>
2022-11-23 22:29:43 +0200

    change message

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Tabular/i);
+  const linkElement = screen.getByText(/Tabular Data/i);
   expect(linkElement).toBeInTheDocument();
 });
