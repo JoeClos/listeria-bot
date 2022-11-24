097a18ced0a9000141aab6f38c6328b35e517421
Josephine <josephine.closan@yahoo.com>
2022-11-24 11:09:15 +0200

    change message

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Listeria/i);
+  const linkElement = screen.getByText(/Data from/i);
   expect(linkElement).toBeInTheDocument();
 });
